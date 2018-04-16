a8acae159e8ad2c2c17748c0663d8ad3baea759d
Weronika <weronika.siedlaczek@gmail.com>
2018-04-16 19:05:43 +0200

    fix : SAGDD-56 : fix UserController

--- src/main/java/com/ksundaysky/controller/LoginController.java
+++ src/main/java/com/ksundaysky/controller/LoginController.java
@@ -71,7 +71,7 @@ public class LoginController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = userService.findUserByEmail(auth.getName());
         modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        modelAndView.addObject("adminMessage","Content Available Only for users with Admin Role");
         modelAndView.setViewName("/home");
         return modelAndView;
     }
--- src/main/java/com/ksundaysky/controller/UserController.java
+++ src/main/java/com/ksundaysky/controller/UserController.java
@@ -1,18 +1,20 @@
 package com.ksundaysky.controller;
 
 import com.ksundaysky.model.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class UserController {
 
-    @RequestMapping(value = {"/admin/user/create"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/users/create"}, method = RequestMethod.GET)
     public ModelAndView createNewUser() {
         ModelAndView modelAndView = new ModelAndView();
         User user = new User();
         modelAndView.addObject("user", user);
-        modelAndView.setViewName("admin/user/create");
+        modelAndView.setViewName("/users/create");
         return modelAndView;
     }
 
